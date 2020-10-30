76268e35a13d194ad28c79c7b879dc8e1d3ef2ab
tinabeattysmith <tinatest2020@gmail.com>
2020-10-30 16:02:08 -0400

    TBS, booklistcontroller, add updatelist

--- controllers/bookListController.js
+++ controllers/bookListController.js
@@ -29,51 +29,51 @@ bookListController.post("/newList", async (req, res) => {
   }
 });
 
-//TBS 10.29.2020: Add Display All list 
+//TBS 10.29.2020: Add Display All list
 /***********************
- * DISPLAY ALL LISTS FOR OWNER 
+ * DISPLAY ALL LISTS FOR OWNER
  ***********************/
 bookListController.get("/allLists", async (req, res) => {
   const bookListOwner = req.user.id;
-  
+
   try {
     let allLists = await BookListModel.findAll({
-      where:
-        {owner : bookListOwner}
-      }).then(
-        function findAllSuccess(allLists) {
-          if(allList != null){
-            res.json(allList)
-          } else res.status(501).json({message: "No lists were found."});        
-        });
+      where: { owner: bookListOwner },
+    }).then((allLists) => {
+      // if list(s) returned, display the list(s); else display message
+      if (allLists.length !== 0) {
+        res.status(200).json(allLists);
+      } else {
+        res.status(204).json({ message: "No lists found." });
+      }
+    });
   } catch (err) {
     {
       res.status(500).json({
-        message: "Failed to retrieve lists.",
+        message: "Failed to retrieve lists",
       });
     }
   }
 });
 
-//TBS 10.29.2020: Add Display Single list 
+//TBS 10.29.2020: Add Display Single list
 /***********************
- * DISPLAY SINGLE LIST FOR OWNER 
+ * DISPLAY SINGLE LIST FOR OWNER
  ***********************/
 bookListController.get("/singleList/:id", async (req, res) => {
   const listID = req.params.id;
   const listOwner = req.user.id;
-  
+
   try {
     let singleList = await BookListModel.findOne({
-      where:
-        {id: listID, owner: listOwner}
-    }).then(
-      function findAllSuccess(singleList) {
-          res.json(singleList);
-          // TODO handle no lists exist
-      // },
-      // function findAllError(err) {
-      //     res.send(500, err.message);
+      where: { id: listID, owner: listOwner },
+    }).then((singleList) => {
+        // if list returned, display the list; else display message
+        if (singleList !== null) {
+          res.status(200).json(singleList);
+        } else {
+          res.status(204).json({ message: "List not found." });
+        }
       });
   } catch (err) {
     {
@@ -82,6 +82,38 @@ bookListController.get("/singleList/:id", async (req, res) => {
       });
     }
   }
+});
+
+//TBS 10.30.2020: Add Update list
+/***********************
+ * UPDATE LIST
+ ***********************/
+bookListController.put("/updateList/:id", async (req, res) => {
+  const listID = req.params.id;
+  const listOwner = req.user.id;
+  const listTitle = req.body.title;
+
+  try {
+    let updateList = await BookListModel.update(
+      {title: listTitle},
+      {where: 
+        {id: listID, owner: listOwner}
+      });
+      // if (updateList && title) {
+      //   updateList.title = title;
+      //   await updateList.save();
+        res.json({
+          message: 'Log was successfully updated.'
+        });
+      // } else 
+      // {
+      //   res.status(404).json({
+      //     message: "List title missing",
+      //   });
+      } catch { function updateError(err) {
+      res.send(500, err.message)
+      };
+    }  
 });
 
 module.exports = bookListController;
