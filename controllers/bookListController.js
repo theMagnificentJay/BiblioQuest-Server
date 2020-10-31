3e5924dfe817a50c4abd45daddfbd08d799b9f51
tinabeattysmith <tinatest2020@gmail.com>
2020-10-31 15:02:27 -0400

    TBS booklistcontroller Updated PUT functionality

--- controllers/bookListController.js
+++ controllers/bookListController.js
@@ -41,10 +41,10 @@ bookListController.get("/allLists", async (req, res) => {
       where: { owner: bookListOwner },
     }).then((allLists) => {
       // if list(s) returned, display the list(s); else display message
-      if (allLists.length !== 0) {
+      if (allLists.length > 0) {
         res.status(200).json(allLists);
       } else {
-        res.status(204).json({ message: "No lists found." });
+        res.status(404).json({ message: "No lists found." });
       }
     });
   } catch (err) {
@@ -58,7 +58,7 @@ bookListController.get("/allLists", async (req, res) => {
 
 //TBS 10.29.2020: Add Display Single list
 /***********************
- * DISPLAY SINGLE LIST FOR OWNER
+ * DISPLAY SINGLE LIST
  ***********************/
 bookListController.get("/singleList/:id", async (req, res) => {
   const listID = req.params.id;
@@ -68,13 +68,13 @@ bookListController.get("/singleList/:id", async (req, res) => {
     let singleList = await BookListModel.findOne({
       where: { id: listID, owner: listOwner },
     }).then((singleList) => {
-        // if list returned, display the list; else display message
-        if (singleList !== null) {
-          res.status(200).json(singleList);
-        } else {
-          res.status(204).json({ message: "List not found." });
-        }
-      });
+      // if list returned, display the list; else display message
+      if (singleList !== null) {
+        res.status(200).json(singleList);
+      } else {
+        res.status(404).json({ message: "List not found." });
+      }
+    });
   } catch (err) {
     {
       res.status(500).json({
@@ -88,32 +88,30 @@ bookListController.get("/singleList/:id", async (req, res) => {
 /***********************
  * UPDATE LIST
  ***********************/
-bookListController.put("/updateList/:id", async (req, res) => {
+bookListController.put("/update/:id", async (req, res) => {
   const listID = req.params.id;
   const listOwner = req.user.id;
-  const listTitle = req.body.title;
+  const updatedTitle = req.body.title;
 
   try {
-    let updateList = await BookListModel.update(
-      {title: listTitle},
-      {where: 
-        {id: listID, owner: listOwner}
-      });
-      // if (updateList && title) {
-      //   updateList.title = title;
-      //   await updateList.save();
-        res.json({
-          message: 'Log was successfully updated.'
-        });
-      // } else 
-      // {
-      //   res.status(404).json({
-      //     message: "List title missing",
-      //   });
-      } catch { function updateError(err) {
-      res.send(500, err.message)
-      };
-    }  
+    // select a list where id = listID and owner = listowner
+    let updateList = await BookListModel.findOne(
+      { where: { id: listID, owner: listOwner },
+     });
+    //  if updatelist and updatedTitle both exist,
+    //  update title to updatedTitle, and respond with status(200) and message.
+    //  else respond with status(404) and message.
+     if (updateList && updatedTitle) {
+        updateList.update({title: updatedTitle});
+          res.status(200).json({message: "List successfully updated"});
+       } else {
+        res.status(404).json({message: "List not found or update unsuccessful."})
+     };  
+  } catch {
+    function updateError(err) {
+      res.send(500, err.message);
+    }
+  }
 });
 
 module.exports = bookListController;
